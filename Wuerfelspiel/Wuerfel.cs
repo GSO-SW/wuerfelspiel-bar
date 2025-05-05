5dbb9fe68a9d92a8861f85511c9ab625cad5c7d2
Nikhil <nikhilkumarjan39@gmail.com>
2025-05-05 10:56:28 +0200

    Tests Gefixxt

--- Wuerfelspiel/Wuerfel.cs
+++ Wuerfelspiel/Wuerfel.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace Wuerfelspiel
 {
     public class Wuerfel
     {
-        private int 
+        int anzahlSeiten;
+        public Wuerfel(int anzahlSeiten) { this.anzahlSeiten = anzahlSeiten; }
+
+        public int Wuerfeln()
+        {
+            return 1; 
+        }
     }
 }
--- WuerfelspielTests/WuerfelTests.cs
+++ WuerfelspielTests/WuerfelTests.cs
@@ -7,20 +7,20 @@ namespace WuerfelspielTests
     [TestClass]
     public class WuerfelTests
     {
-        [TestMethod]
         [DataRow(1)]
         [DataRow(2)]
         [DataRow(3)]
         [DataRow(4)]
         [DataRow(5)]
         [DataRow(6)]
-        public int Wuerfel_wuerfeltZahlZwischenEinsUndSechs
+        [TestMethod]
+        public void Wuerfel_wuerfeltZahlZwischenEinsUndSechs(int moeglicheSumme)
         {
             // Arrange
             Wuerfel wuerfel = new Wuerfel(6);
 
             // Act
-            int summe = wuerfel.Wuefeln();
+            int summe = wuerfel.Wuerfeln();
 
             // Assert
             Assert.AreEqual(moeglicheSumme, summe);
